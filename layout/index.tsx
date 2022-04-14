fedddcbf6963310ec7f5325404b6d78663f3eb3f
Gregory Quinio <1063530+undefined@users.noreply.github.com>
2022-04-14 12:12:00 +0000

    Push from removeLogo

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,13 +1,11 @@
 /* @jsx h */
 import { h } from '@stencil/core';
 import Layout from '@divriots/dockit-stencil/layout';
+import Logo from './Logo';
 
 export default ({ __context }, children) => (
   <div style={{ '--dockit-layout-accent': 'rgb(238,82,82)' }}>
-    <Layout
-      __context={__context}
-      logoURL="https://dam.malt.com/rebranding2020/malt-logo/malt-red"
-    >
+    <Layout __context={__context} logo={Logo}>
       {children}
     </Layout>
   </div>
